d98a6b5362a956aaccbfb82766814e61ef0b3d54
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-29 20:03:41 +0100

    feat: added patch and remove

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param, Patch,
+  Post,
+  Query
+} from "@nestjs/common";
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Controller('auth')
 export class UsersController {
@@ -19,5 +28,15 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
