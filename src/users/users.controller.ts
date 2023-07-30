3e80e0b92f7a5911d159e09148e775c0b5a57861
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-30 16:46:22 +0100

    feat: added endpoints to test cookies

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Query,
+  Session,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -22,6 +23,18 @@ export class UsersController {
     private readonly usersService: UsersService,
     private readonly authService: AuthService,
   ) {}
+
+  @Get('/colors/:color')
+  setColor(@Param('color') color: string, @Session() session: any) {
+    console.log(session);
+    session.color = color;
+  }
+
+  @Get('/colors')
+  getColor(@Session() session: any) {
+    console.log(session);
+    return session.color;
+  }
 
   @Post('/signup')
   create(@Body() createUserDto: CreateUserDto) {
