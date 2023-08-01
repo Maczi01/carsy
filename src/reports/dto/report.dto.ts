37ddef21ad03d6936b6b60f61011fac37a3b5a6d
maczi01 <mateusz.w.twardy@gmail.com>
2023-08-01 22:58:39 +0100

    feat: changed response in create report (not showing password anymore)

---
+++ src/reports/dto/report.dto.ts
@@ -0,0 +1,23 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id: string;
+  @Expose()
+  price: number;
+  @Expose()
+  year: number;
+  @Expose()
+  lng: number;
+  @Expose()
+  lat: number;
+  @Expose()
+  make: string;
+  @Expose()
+  model: string;
+  @Expose()
+  mileage: number;
+  @Expose()
+  @Transform(({ obj }) => obj.user.id)
+  userId: string;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { CreateReportDto } from './dto/create-report.dto';
 import { AuthGuards } from '../guards/auth.guards';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/entities/user.entity';
+import { Serialize } from '../serialize.interceptor/serialize.interceptor';
+import { ReportDto } from './dto/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuards)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
