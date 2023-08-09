517bcfa444d7f4413edd155d830195c367a0b8e7
maczi01 <mateusz.w.twardy@gmail.com>
2023-08-09 22:22:17 +0100

    feat: applied middleware

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,15 +1,29 @@
-import { NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
+@Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
     const { id } = req.session || {};
-    // if (userId) {
-    //   const user = await this.usersService.findOne(userId);
-    //   req?.currentUser = user;
-    // }
+
+    if (id) {
+      const user = await this.usersService.findOne(parseInt(id));
+      req.currentUser = user;
+    }
+
+    next();
   }
 }
